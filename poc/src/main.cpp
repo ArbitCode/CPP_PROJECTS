fb7385cff5916796f9572a397c5fe0aec7f57a65
Rajaram <rajaramsharma422@gmail.com>
2022-11-27 20:00:44 +0530

    added print line (#5)

--- poc/src/main.cpp
+++ poc/src/main.cpp
@@ -4,17 +4,20 @@
 void complex_bar(){
     Complex a = Complex();
     Complex b = Complex();
+    std::cout << "-------------" << std::endl;
     std::cout << "a: ";
     a.displayData();
     std::cout << "b: ";
     b.displayData();
+    std::cout << "-------------" << std::endl;
     std:: cout << "a + b = ";
     Complex c = a + b;
     c.displayData();
+    std::cout << "-------------" << std::endl;
 }
 
 int main(){
     std::cout << "--* POC program *--" << std::endl;
     complex_bar();
-    std::cout << "*--- End ---*";
+    std::cout << "*--- End ---*" << std::endl;
 }
